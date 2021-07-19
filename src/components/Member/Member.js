ffdb44e2f5874d4b7e8b4482f843a32d4b67d7c5
Rusdi <rusdi.karsandi@gmail.com>
2021-07-20 01:16:22 +0700

    repair member and hit api with axios but still cannot display data

--- src/components/Member/Member.js
+++ src/components/Member/Member.js
@@ -1,17 +1,62 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import Admin from './Admin';
+// import { useParams } from 'react-router-dom';
+// import Admin from './Admin';
+import axios from 'axios';
 
-export default function Member() {
+var response;
+class Member extends React.Component {
     // const { search } = useLocation();
     // const match = search.match(/type=(.*)/);
     // const type = match?.[1];
-    const { type } = useParams();
+    // const { type } = useParams();
 
-    return (
-        <>
-        <h2>Member</h2>
-        {type === 'admin' && <Admin />}
-        </>
-    );
+
+    constructor(){
+        super();
+        this.state = {
+            serverResponse: '',
+            data: null
+        }
+    }
+
+    async getData() {
+
+        try {
+            const apiUrl = 'http://www.grevia.com/api/member?secretkey=grevia';
+            // const apiUrl = 'http://localhost/greviacom/api/member?secretkey=grevia';
+    
+            const res = await axios.get(apiUrl);
+            return await res.json();
+
+        } catch(e) {
+            console.log('request failed ${e}')
+        }
+    }
+
+    componentDidMount(){
+        if (!this.state.data) {
+            (async () => {
+                try {
+                    this.setState({data: await this.getData()})
+                    console.log('woohoo hit data axios');
+                    console.log(this.state.data);
+                } catch (e) {
+                    console.log('waduh error ngab ${e}');
+                }
+            })();
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            <h2>Member</h2>
+            
+            { this.state.data ? <em>Loading...</em> : this.state.data}
+            </div>
+        )
+    }
 }
+export default Member;
